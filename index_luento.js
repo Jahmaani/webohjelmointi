e5b16672da14175aa19998542c1ec32b9877b8f8
Tomi Väisänen <Tomi.Vaisanen@edu.savonia.fi>
2020-11-03 13:34:23 +0200

    toinen lisäys

--- index_luento.js
+++ index_luento.js
@@ -23,5 +23,6 @@ http.createServer(function(request, response){
     response.writeHead(200, {'Content-Type' : 'text/plain'});
     response.end("Hello World!");*/
 }).listen(3002);
+//dasdads
 
 console.log("Server running at http://localhost:3002");
